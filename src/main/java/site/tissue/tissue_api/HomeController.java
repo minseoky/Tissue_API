12d9ef86171ca9458f95b3695a88d89de9fa65dc
minseoky <minseok184@gmail.com>
2023-08-23 15:23:27 +0900

    [#05] react 연동 prototype

--- src/main/java/site/tissue/tissue_api/HomeController.java
+++ src/main/java/site/tissue/tissue_api/HomeController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
+/*
 @RestController
 public class HomeController {
     @GetMapping("/")
@@ -12,3 +13,6 @@ public class HomeController {
     }
 
 }
+
+*/
+
--- src/main/java/site/tissue/tissue_api/WebConfig.java
+++ src/main/java/site/tissue/tissue_api/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 대해 CORS 허용
-                .allowedOrigins("http://localhost:3000", "http://192.168.0.52:3000") // 허용할 origin 설정
+                .allowedOrigins("http://localhost:3000") // 허용할 origin 설정
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메서드 설정
                 .allowCredentials(true); // 필요에 따라 credentials 허용 설정
     }
